9fe485710cec1a5cb995ce8fdb2de1ff5fc476b9
zac1st1k <zacfirst@gmail.com>
2014-12-12 22:46:37 +1100

    Initial Commit

---
+++ NSUserDefaults, Segues and Protocols Chanllenge SolutionTests/NSUserDefaults__Segues_and_Protocols_Chanllenge_SolutionTests.m
@@ -0,0 +1,34 @@
+//
+//  NSUserDefaults__Segues_and_Protocols_Chanllenge_SolutionTests.m
+//  NSUserDefaults, Segues and Protocols Chanllenge SolutionTests
+//
+//  Created by Zac on 12/12/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSUserDefaults__Segues_and_Protocols_Chanllenge_SolutionTests : XCTestCase
+
+@end
+
+@implementation NSUserDefaults__Segues_and_Protocols_Chanllenge_SolutionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
